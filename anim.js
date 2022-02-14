ae57ac41fb0d0cff35ae95ab8ee6894b57993474
kaudaouda <konedaouda7894@yahoo.com>
2022-02-14 17:28:39 +0000

    final update

--- anim.js
+++ anim.js
@@ -70,4 +70,18 @@ document.addEventListener('DOMContentLoaded', function() {
         timer=setTimeout(mouseStopped,500)
      })
   })
+  const menuBtn = document.querySelector ('.menu-btn')
+  const menuMobile = document.querySelector('.second_header')
+  let menuOpen = false;
+  menuBtn.addEventListener('click', ()=> {
+    if (!menuOpen) {
+      menuBtn.classList.add('open')
+      menuMobile.style.display = 'flex'
+      menuOpen = true;
+    } else{
+      menuBtn.classList.remove('open')
+      menuMobile.style.display = 'none'
+      menuOpen = false;
+    }
+  })
 })
