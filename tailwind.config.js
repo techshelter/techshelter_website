e9ed213567303dd0bb1ddfd892ff5eeb52ad5d83
kaudaouda <konedaouda7894@yahoo.com>
2022-03-03 12:38:06 +0000

    modification

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./index.html", "./node_modules/flowbite/**/*.js"], 
   theme: {
     extend: {},
+    fontFamily: {
+        'handlee': ['"handlee"']
+      }
   },
   plugins: [
     require('flowbite/plugin')
